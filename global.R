a530ae4e340e8566ac4d9d75275d9f48658b2388
ZenanXing <zxing001@ucr.edu>
2026-08-20 09:51:14 -0700

    Fix NA coercion error in display formatting

--- global.R
+++ global.R
@@ -1,8 +1,17 @@
 display_format <- function(val) {
-  if (val != "/" && !is.na(val)) {
-    val <- as.numeric(val)
-    return(ifelse(val < 0.01, formatC(val, format = "E", digits = 2), format(round(val, digits = 2), nsmall = 2)))
+  if (is.na(val)) {
+    return(NA_character_)
+  }
+  
+  if (val == "/") {
+    return("/")
+  }
+  
+  val <- as.numeric(val)
+  
+  if (val < 0.01) {
+    formatC(val, format = "E", digits = 2)
   } else {
-    return(val)
+    format(round(val, digits = 2), nsmall = 2)
   }
 }
